e86a32eca1543928a3b46f7d16d6dccd9852c5e3
li.fusheng <leyoumake@gmail.com>
2022-05-24 15:31:27 +0800

    join `ReqResponseConvToBytesAndGetHeaders` func

--- imroc_req_v3_conv.go
+++ imroc_req_v3_conv.go
@@ -1,6 +1,10 @@
 package imroc_req_v3_conv
 
-import v3 `github.com/imroc/req/v3`
+import (
+	"net/http"
+
+	v3 "github.com/imroc/req/v3"
+)
 
 // 将请求响应转`[]byte`
 func ReqResponseConvToBytes(receiveResponse *v3.Response, receiveErr error) ([]byte, error) {
@@ -8,6 +12,14 @@ func ReqResponseConvToBytes(receiveResponse *v3.Response, receiveErr error) ([]b
 		return []byte{}, receiveErr
 	}
 	return receiveResponse.Bytes(), nil
+}
+
+// 将请求响应转`[]byte`，并且获取响应头
+func ReqResponseConvToBytesAndGetHeaders(receiveResponse *v3.Response, receiveErr error) (http.Header, []byte, error) {
+	if receiveErr != nil {
+		return nil, []byte{}, receiveErr
+	}
+	return receiveResponse.Header, receiveResponse.Bytes(), nil
 }
 
 // 将请求响应转`string`
